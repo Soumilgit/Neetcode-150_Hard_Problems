0795671f0187df6017a5f50b7523ba7da3ab2905
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-07-21 18:40:14 +0530

    Create Duplicate_Integer.py

---
+++ Easy/Duplicate_Integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n-1):
+            
+            for j in range (i+1,n):
+                if nums[i]==nums[j]:
+                    return True
+        return False
+         
